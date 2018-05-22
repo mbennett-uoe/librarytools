6f5af639a90dd64ddad4909c9272ab8707292d9d
Mike Bennett <mike.bennett@ed.ac.uk>
2018-05-22 09:55:05 +0100

    datecounter utility

---
+++ datecounter/datecounter.py
@@ -0,0 +1,22 @@
+import os, sys
+from datetime import datetime
+
+results = {}
+if len(sys.argv) > 1:
+    if os.path.isdir(sys.argv[1]):
+        dirpath = sys.argv[1]
+    else:
+        sys.exit("Directory does not exist")
+else:
+    dirpath = os.getcwd()
+
+for f in os.listdir(dirpath):
+    fpath = os.path.join(dirpath, f)
+    dt = datetime.fromtimestamp(os.path.getmtime(fpath)).date()
+    if dt in results.keys():
+        results[dt] = results[dt] + 1
+    else:
+        results[dt] = 1
+
+for result, val in sorted(results.iteritems()):
+    print result.strftime("%a %d %b %Y"), ":", val
